d6b4378af4e2cc372f901121201eaccc559b0bae
Hanbee Lee <bee1162@naver.com>
2022-11-17 22:47:21 +0900

    fix:merge

--- nextjs-intro/hooks/useInput.js
+++ nextjs-intro/hooks/useInput.js
@@ -6,23 +6,26 @@ export default function useInput() {
   const NEXT_PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
   const handleChange = (e) => {
-    setSearchValue(e.target.value);
-    tryOut(e);
+    if (!e.target.value) {
+      resetChat();
+    } else {
+      setSearchValue(e.target.value);
+      tryOut(e);
+    }
   };
 
   const resetChat = () => {
     setSearchValue('');
     setSearchResults('');
   };
-
   const tryOut = async (e) => {
     e.preventDefault();
+
     const movies = await (
       await fetch(
         `https://api.themoviedb.org/3/search/movie?api_key=${NEXT_PUBLIC_API_KEY}&language=en-US&query=${e.target.value}&include_adult=false`
       )
     ).json();
-
     setSearchResults(movies);
   };
 
